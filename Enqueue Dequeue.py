bb94fbef430b8c6ae2c3f98ea0c382ca2ba73b76
Jarukorn Thuengjitvilas <Jarukorn Thuengjitvilas>
2017-03-15 10:58:47 +0700

    Linkedlist & Push Pop

---
+++ Enqueue Dequeue.py
@@ -0,0 +1,24 @@
+
+head = 0
+tail = 0
+
+
+def enqueue(Q,x):
+    global head, tail
+    Q[tail] = x;
+    if tail == len(Q) :
+        tail = 0
+    else:
+        tail -= 1
+
+def dequeue (Q):
+    global head, tail
+    x = Q[head]
+    if head == len(Q):
+        head = 0
+    else:
+        head += 1
+        return x
+
+Q = [0 for x in range(10)]
+print(dequeue(Q))
---
+++ Linked List/LinkedList.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, k):
+        self.prev = None
+        self.next = None
+        self.key = k
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+def list_search(L,k):
+    x = L.head
+    while x != None and x.key != k:
+        x = x.next
+    return x
+
+def list_insert(L,x):
+    x.next = L.head
+    if L.head != None:
+        L.head.prev = x
+    L.head = x
+    x.prev = None
+def list_delete(L,x):
+    if x.prev != None:
+        x.prev.next = x.next
+    else :
+        L.head = x.next
+    if x.next != None:
+        x.next.prev = x.prev
+
+L = LinkedList()
+list_insert(L,Node(10))
+list_insert(L,Node(1))
+result = list_search(L,10)
+print (result.key)
+result = list_search(L,2)
+print (result)
